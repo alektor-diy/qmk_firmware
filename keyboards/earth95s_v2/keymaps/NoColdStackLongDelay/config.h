28e9e09dd0f23ab3d23f798ce015c9b0f01aa2e2
アレくとーる <alektordiy@gmail.com>
2025-10-06 03:19:22 +0900

    update

---
+++ keyboards/earth95s_v2/keymaps/NoColdStackLongDelay/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+
+//rp2040ボードで起動時にコールドスタックする事象に対する対応
+#define SPLIT_USB_TIMEOUT 10000 // default 2000
+#define SPLIT_USB_TIMEOUT_POLL 25 // default 10
